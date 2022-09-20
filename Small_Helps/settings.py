e1ec6c31fc3b4aee1e3f7b4bf1169e79d6596554
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-09-20 19:09:46 +0600

    mysql connected

--- Small_Helps/settings.py
+++ Small_Helps/settings.py
@@ -65,8 +65,13 @@ WSGI_APPLICATION = 'Small_Helps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'small-helps',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
